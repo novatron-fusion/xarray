af999049da206c591e9e2a7f6138f2542ec6fc54
Jonas Grönberg <jonas.gronberg@novatronfusion.com>
2025-06-03 14:25:43 +0200

    WIP: moved fetching of branch (root.group[path]) to next thread.

--- xarray/backends/h5netcdf_.py
+++ xarray/backends/h5netcdf_.py
@@ -588,43 +588,6 @@ class H5netcdfBackendEntrypoint(BackendEntrypoint):
                     decode_timedelta=decode_timedelta,
                 )
             return group_ds
-        
-        def _find_branches(executor: concurrent.futures.Executor, root, parent="/"):
-            from xarray.core.treenode import NodePath
-            parent = NodePath(parent)
-            futures = {}
-            for path, group in root.groups.items():
-                gpath = parent / path
-                future = executor.submit(_find_branches, executor, group, gpath)
-                futures[future] = str(gpath)
-            return futures
-        
-        def _build_tree_paths(root, parent):
-            parent = str(parent)
-            max_iterations = 100
-            thread_count = 30
-
-            tree = []
-            iterations = 0
-            with concurrent.futures.ThreadPoolExecutor(max_workers=thread_count) as executor:
-                root_future = executor.submit(_find_branches, executor, root, parent)
-                futures={root_future: parent}
-                while futures and iterations<max_iterations:
-                    iterations += 1
-                    if iterations>=max_iterations:
-                        print(f"Max number of iterations ({max_iterations}) reached")
-                    new_futures = {}
-                    for future in concurrent.futures.as_completed(futures):
-                        branch_path = str(futures[future])
-                        tree.append(branch_path)
-                        try:
-                            new_future = future.result()
-                        except Exception as exc:
-                            print(f"Thread executing find_branches with parent: {str(parent)} raised error: {str(exc)}")
-                            raise
-                        new_futures.update(new_future)
-                    futures = new_futures
-            return tree
 
         # Keep this message for some versions
         # remove and set phony_dims="access" above
@@ -663,7 +626,7 @@ class H5netcdfBackendEntrypoint(BackendEntrypoint):
         else:
             print("Run in Parallel")
             root_ds = store.ds
-            tree_paths = _build_tree_paths(root=root_ds, parent=parent)
+            tree_paths = _build_tree_paths_alt(root=root_ds, parent=parent)
             thread_count = 30
             with concurrent.futures.ThreadPoolExecutor(
                 max_workers=thread_count
@@ -698,5 +661,85 @@ class H5netcdfBackendEntrypoint(BackendEntrypoint):
 
         return groups_dict
 
+
+
+def _find_branches(executor: concurrent.futures.Executor, root, parent="/"):
+    from xarray.core.treenode import NodePath
+    parent = NodePath(parent)
+    futures = {}
+    for path, group in root.groups.items():
+        gpath = parent / path
+        future = executor.submit(_find_branches, executor, group, gpath)
+        futures[future] = str(gpath)
+    return futures
+
+def _build_tree_paths(root, parent):
+    parent = str(parent)
+    max_iterations = 100
+    thread_count = 30
+
+    tree = []
+    iterations = 0
+    with concurrent.futures.ThreadPoolExecutor(max_workers=thread_count) as executor:
+        root_future = executor.submit(_find_branches, executor, root, parent)
+        futures={root_future: parent}
+        while futures and iterations<max_iterations:
+            iterations += 1
+            if iterations>=max_iterations:
+                print(f"Max number of iterations ({max_iterations}) reached")
+            new_futures = {}
+            for future in concurrent.futures.as_completed(futures):
+                branch_path = str(futures[future])
+                tree.append(branch_path)
+                try:
+                    new_future = future.result()
+                except Exception as exc:
+                    print(f"Thread executing find_branches with parent: {str(parent)} raised error: {str(exc)}")
+                    raise
+                new_futures.update(new_future)
+            futures = new_futures
+    return tree
+
+def _find_branches_alt(executor: concurrent.futures.Executor, next_path=None, root=None, parent="/"):
+    from xarray.core.treenode import NodePath
+    parent = NodePath(parent)
+    if not next_path:
+        group = root
+    else:
+        group = root.groups[next_path]
+    
+    futures = {}
+    for path in group.groups.keys():
+        gpath = parent / path
+        future = executor.submit(_find_branches_alt, executor, next_path=path, root=group, parent=gpath)
+        futures[future] = str(gpath)
+    return futures
+
+def _build_tree_paths_alt(root, parent):
+    parent = str(parent)
+    max_iterations = 100
+    thread_count = 30
+
+    tree = []
+    iterations = 0
+    with concurrent.futures.ThreadPoolExecutor(max_workers=thread_count) as executor:
+        root_future = executor.submit(_find_branches_alt, executor, root=root, parent=parent)
+        futures={root_future: parent}
+        while futures and iterations<max_iterations:
+            iterations += 1
+            if iterations>=max_iterations:
+                print(f"Max number of iterations ({max_iterations}) reached")
+            new_futures = {}
+            for future in concurrent.futures.as_completed(futures):
+                branch_path = str(futures[future])
+                tree.append(branch_path)
+                try:
+                    new_future = future.result()
+                except Exception as exc:
+                    print(f"Thread executing find_branches with parent: {str(parent)} raised error: {str(exc)}")
+                    raise
+                new_futures.update(new_future)
+            futures = new_futures
+    return tree
 
 BACKEND_ENTRYPOINTS["h5netcdf"] = ("h5netcdf", H5netcdfBackendEntrypoint)
